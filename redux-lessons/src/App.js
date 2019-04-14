a52affd8c1453288508ba7fc0c0066ce83ad342b
Marek <marekficht@gmail.com>
2019-04-14 20:54:35 +0200

    lesson: react-redux + structure + connet()

--- redux-lessons/src/App.js
+++ redux-lessons/src/App.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-// import { createStore, combineReducers, bindActionCreators } from 'redux';
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers'
-
-import { movieActions } from './app/movies/duck'
-
+import MoviesContainer from './app/movies/components/MoviesContainer'
 
 /**
  * https://redux.js.org/introduction/getting-started
@@ -19,38 +11,16 @@ import { movieActions } from './app/movies/duck'
  * 5. Kreatory akcji - zwykla funckja ktora opakowuje obiekt w dispatch, co by bylo czytelniej
  * 5a. bindActionCreators - fajniejszy sposob na rozdzielanie akcji, zeby nie spamowac 'store.dispatch'
  * 6. Struktura projektu - 
+ * 7. react-redux - 
  * 
  */
-
-//--- 6. New clear structure
-const store = createStore(rootReducer, composeWithDevTools());
-window.store = store;
-
-store.dispatch( movieActions.add('Avatar') );
-
-
-/** 6. */
-console.log( window.store.getState() );
 
 //---
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MoviesContainer />
       </div>
     );
   }
